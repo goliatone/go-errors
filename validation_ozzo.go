340f5dc55ee0398d1806225998886016a2935b74
goliatone <140582+goliatone@users.noreply.github.com>
2025-05-14 17:25:33 -0700

    add: ozzo validation helper

---
+++ validation_ozzo.go
@@ -0,0 +1,62 @@
+package errors
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+func FromOzzoValidation(err error, message string) *Error {
+	if err == nil {
+		return nil
+	}
+
+	if validationErrors, ok := err.(validation.Errors); ok {
+		return fromOzzoValidationErrors(validationErrors, message)
+	}
+
+	// other types create a general validation error
+	return &Error{
+		Category:  CategoryValidation,
+		Message:   message,
+		Source:    err,
+		Timestamp: time.Now(),
+	}
+}
+
+func fromOzzoValidationErrors(validationErrors validation.Errors, message string) *Error {
+	var fieldErrors ValidationErrors
+
+	for field, fieldErr := range validationErrors {
+		if nestedErrors, ok := fieldErr.(validation.Errors); ok {
+			for nestedField, nestedErr := range nestedErrors {
+				fieldName := fmt.Sprintf("%s.%s", field, nestedField)
+				fieldErrors = append(fieldErrors, FieldError{
+					Field:   fieldName,
+					Message: strings.TrimSpace(nestedErr.Error()),
+				})
+			}
+		} else {
+			fieldErrors = append(fieldErrors, FieldError{
+				Field:   field,
+				Message: strings.TrimSpace(fieldErr.Error()),
+			})
+		}
+	}
+
+	return &Error{
+		Category:         CategoryValidation,
+		Message:          message,
+		ValidationErrors: fieldErrors,
+		Timestamp:        time.Now(),
+	}
+}
+
+func ValidateWithOzzo(validateFunc func() error, message string) *Error {
+	if err := validateFunc(); err != nil {
+		return FromOzzoValidation(err, message)
+	}
+	return nil
+}
