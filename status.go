c967a795856a45a6ba75297ccc86b9b75dba3b4f
goliatone <140582+goliatone@users.noreply.github.com>
2025-07-03 20:36:52 -0700

    add: new status codes

--- status.go
+++ status.go
@@ -3,10 +3,12 @@ package errors
 import "net/http"
 
 const (
-	CodeNotFound       = http.StatusNotFound
-	CodeConflict       = http.StatusConflict
-	CodeBadRequest     = http.StatusBadRequest
-	CodeForbidden      = http.StatusForbidden
-	CodeInternal       = http.StatusInternalServerError
-	CodeRequestTimeout = http.StatusRequestTimeout
+	CodeNotFound        = http.StatusNotFound
+	CodeConflict        = http.StatusConflict
+	CodeBadRequest      = http.StatusBadRequest
+	CodeForbidden       = http.StatusForbidden
+	CodeInternal        = http.StatusInternalServerError
+	CodeUnauthorized    = http.StatusUnauthorized
+	CodeRequestTimeout  = http.StatusRequestTimeout
+	CodeTooManyRequests = http.StatusTooManyRequests
 )
