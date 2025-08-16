a031aad036577fc75cc5011ca96c4b51b8d3a86d
goliatone <140582+goliatone@users.noreply.github.com>
2025-08-16 11:51:53 -0700

    add: severity

--- logger.go
+++ logger.go
@@ -20,6 +20,9 @@ func ToSlogAttributes(err error) []slog.Attr {
 			attrs = append(attrs, slog.String("category", richErr.Category.String()))
 		}
 
+		// Add severity information
+		attrs = append(attrs, slog.String("severity", richErr.Severity.String()))
+
 		if richErr.RequestID != "" {
 			attrs = append(attrs, slog.String("request_id", richErr.RequestID))
 		}
@@ -34,4 +37,35 @@ func ToSlogAttributes(err error) []slog.Attr {
 		return attrs
 	}
 	return nil
+}
+
+// LogBySeverity logs an error using the appropriate slog level based on its severity
+func LogBySeverity(logger *slog.Logger, err *Error) {
+	if logger == nil || err == nil {
+		return
+	}
+
+	attrs := ToSlogAttributes(err)
+	// Convert []slog.Attr to []any for logging methods
+	anyAttrs := make([]any, len(attrs))
+	for i, attr := range attrs {
+		anyAttrs[i] = attr
+	}
+
+	switch err.Severity {
+	case SeverityDebug:
+		logger.Debug(err.Error(), anyAttrs...)
+	case SeverityInfo:
+		logger.Info(err.Error(), anyAttrs...)
+	case SeverityWarning:
+		logger.Warn(err.Error(), anyAttrs...)
+	case SeverityError:
+		logger.Error(err.Error(), anyAttrs...)
+	case SeverityCritical:
+		logger.Error(err.ErrorWithStack(), anyAttrs...) // Include stack trace for critical errors
+	case SeverityFatal:
+		logger.Error(err.ErrorWithStack(), anyAttrs...) // Include stack trace for fatal errors
+	default:
+		logger.Error(err.Error(), anyAttrs...)
+	}
 }
